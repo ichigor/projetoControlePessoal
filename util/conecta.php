427197c27757c9a0249847b2077ee323f55c6cd5
ichigor <igor.moiano@hotmail.com>
2017-10-28 15:10:08 -0200

    trocado banco de acesso

--- util/conecta.php
+++ util/conecta.php
@@ -5,8 +5,8 @@
  * Date: 05/10/2017
  * Time: 14:17
  */
-$conexao = mysqli_connect("localhost", "root", "", "projetoIntegrador");
-//$conexao = mysqli_connect("ipobfcpvprjpmdo9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "szsb1h8zi5dw2atz", "zjg1ei8rvnc9vrzo", "xuhitdelpl3u60i6");
+//$conexao = mysqli_connect("localhost", "root", "", "projetoIntegrador");
+$conexao = mysqli_connect("ipobfcpvprjpmdo9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "szsb1h8zi5dw2atz", "zjg1ei8rvnc9vrzo", "xuhitdelpl3u60i6");
 
 if (!$conexao->set_charset("utf8")) {
     exit();
